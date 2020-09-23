1007f18f313612a77a0e72c35c49c2e14b6fdcaa
Olexandr Tymoshenko <o.s.tymoshenko1999@gmail.com>
2020-09-23 16:49:15 +0300

    Lesson 7: Before using docker

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,11 +1,18 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.contrib import admin
 
-from django.contrib import admin
+from .models import Choice, Question
 
-from .models import Question
 
-admin.site.register(Question)
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
 
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
