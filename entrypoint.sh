ef8af35faae7dcd072c266d40f07b438bdcd0c9a
Olexandr Tymoshenko <o.s.tymoshenko1999@gmail.com>
2020-09-24 13:05:01 +0300

    add wait-for-it.sh script

--- entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-python ./mysite/manage.py reset_db --noinput
-python ./mysite/manage.py migrate
-python ./mysite/manage.py add_testing_data
-python ./mysite/manage.py runserver 0.0.0.0:8000
