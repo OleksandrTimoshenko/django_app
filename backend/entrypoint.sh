98ebb79bf7dae2ac5db6b2f32b66c06922d62ae0
Olexandr Tymoshenko <o.s.tymoshenko1999@gmail.com>
2020-09-24 13:29:37 +0300

    renaming the upper mysite to the backend

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-python ./manage.py reset_db --noinput
+#python ./manage.py reset_db --noinput   # ERR: Unknown command: 'reset_db'
 python ./manage.py migrate
 python ./manage.py add_testing_data
 python ./manage.py runserver 0.0.0.0:8000
